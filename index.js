972a3155d00bf4e9b83abf7c654b29ed8ec194fe
Threqt <45086473+Threqt@users.noreply.github.com>
2019-05-24 00:58:23 -0500

    Added delRole command

--- index.js
+++ index.js
@@ -68,6 +68,48 @@ bot.on("message", message => {
       let channel = message.guild.channels.find(`name`, `bot-logs`)
       channel.send(roleEmbed);
     })
+  } else
+  if (cmd === `delrole`) {
+    if (!message.member.hasPermission("MANAGE_MESSAGES")) {
+      return message.reply("Insufficient Permissions.").then(r => r.delete(5000))
+    }
+    if (message.mentions.users.size === 0) {
+      return message.reply("Please mention a user.").then(r => r.delete(5000))
+    }
+    let roleMember = message.guild.member(message.mentions.users.first())
+    if (!roleMember) {
+      return message.reply("Invalid Member").then(r => r.delete(5000))
+    }
+    let role = args.slice(message.mentions.members.size).join(' ')
+    if (!role) {
+      return message.reply('Please add a role.').then(r => r.delete(5000))
+    }
+    let realrole = message.guild.roles.find(`name`, role)
+    if (!realrole) {
+      return message.reply("Please add a valid role").then(r => r.delete(5000))
+    }
+    if (roleMember.highestRole.position < realrole.position) {
+      return message.reply("Role too high to be removed").then(r => r.delete(5000))
+    }
+    if (roleMember.highestRole.position > message.member.highestRole.position) {
+      return message.reply("Cannot role this person!").then(r => r.delete(5000));
+    }
+    const roleEmbed = new Discord.RichEmbed()
+      .setAuthor(`Coruscant Guard Bot`, bot.user.avatarURL)
+      .setThumbnail(bot.user.avatarURL)
+      .setTitle(`ROLE REMOVED BY ` + message.author.username)
+      .setDescription(`LOG`)
+      .addField("User:", roleMember.user.username, true)
+      .addField("Role Removed:", realrole.name, true)
+      .addField("Channel:", message.channel, true)
+      .addField("Time:", message.createdAt, true)
+      .setFooter("Prefix: ! | This bot is still in it's early phases", bot.user.avatarURL)
+      .setTimestamp();
+    roleMember.removeRole(realrole.id).then(messag => {
+      message.reply(`${message.author.username} has removed the role ${realrole.name} from ${roleMember.user.username}`)
+      let channel = message.guild.channels.find(`name`, `bot-logs`)
+      channel.send(roleEmbed);
+    })
   }
 });
 
