c73c52c884d66b84f9511fec402ebff4e74fd9a2
Threqt <45086473+Threqt@users.noreply.github.com>
2019-05-24 00:40:22 -0500

    Added addRole | Changed activity status | Made code neater

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const bot = new Discord.Client({
 
 bot.on("ready", async () => {
   console.log(`${bot.user.username} has been deployed.`)
+  bot.user.setActivity("The Prison | Prefix: !", {
+    type: "WATCHING"
+  });
 })
 
 bot.on("message", message => {
@@ -18,8 +21,52 @@ bot.on("message", message => {
   const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
   const cmd = args.shift().toLowerCase();
 
-  if(cmd === `ping`){
+  if (cmd === `cmds` || cmd === `commands`) {
     message.channel.send("Pong!")
+  } else
+  if (cmd === `addrole`) {
+    if (!message.member.hasPermission("MANAGE_MESSAGES")) {
+      return message.reply("Insufficient Permissions.").then(r => r.delete(5000))
+    }
+    if (message.mentions.users.size === 0) {
+      return message.reply("Please mention a user.").then(r => r.delete(5000))
+    }
+    let roleMember = message.guild.member(message.mentions.users.first())
+    if (!roleMember) {
+      return message.reply("Invalid Member").then(r => r.delete(5000))
+    }
+    let role = args.slice(message.mentions.members.size).join(' ')
+    if (!role) {
+      return message.reply('Please add a role.').then(r => r.delete(5000))
+    }
+    let realrole = message.guild.roles.find(`name`, role)
+    if (!realrole) {
+      return message.reply("Please add a valid role").then(r => r.delete(5000))
+    }
+    if (roleMember.highestRole.position > message.member.highestRole.position) {
+      return message.reply("Cannot role this person!").then(r => r.delete(5000));
+    }
+    if (message.member.highestRole.position < realrole.position) {
+      return message.reply("Role too high to be added!").then(r => r.delete(5000));
+    }
+
+    const roleEmbed = new Discord.RichEmbed()
+      .setAuthor(`Coruscant Guard Bot`, bot.user.avatarURL)
+      .setThumbnail(bot.user.avatarURL)
+      .setTitle(`ROLE ADDED BY ` + message.author.username)
+      .setDescription(`LOG`)
+      .addField("User:", roleMember.user.username, true)
+      .addField("Role Added:", realrole.name, true)
+      .addField("Channel:", message.channel, true)
+      .addField("Time:", message.createdAt, true)
+      .setFooter("Prefix: ! | This bot is still in it's early phases", bot.user.avatarURL)
+      .setTimestamp();
+
+    roleMember.addRole(realrole.id).then(message => {
+      message.reply(`${message.author.username} has added the role ${realrole.name} to ${roleMember.user.username}`)
+      let channel = message.guild.channels.find(`name`, `bot-logs`)
+      channel.send(roleEmbed);
+    })
   }
 });
 
