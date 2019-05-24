0bfe5003efb77f354ef34fa34795f909f88948be
Threqt <45086473+Threqt@users.noreply.github.com>
2019-05-24 01:05:53 -0500

    Fixed cmds, changed it to commands

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ bot.on("message", message => {
   const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
   const cmd = args.shift().toLowerCase();
 
-  if (cmd === `cmds` || cmd === `commands`) {
+  if (cmd === `commands`) {
     const cmdsEmbed = new Discord.RichEmbed()
       .setAuthor(`Coruscant Guard Bot`, bot.user.avatarURL)
       .setThumbnail(bot.user.avatarURL)
@@ -29,6 +29,7 @@ bot.on("message", message => {
       .setDescription("**addrole** | Usage: !addrole User#Discriminator RoleName | Adds the specified role to the specified player. \n \n **delrole** | Usage: !delrole User#Discriminator RoleName | Removes the specified role from the specified user")
       .setFooter("Prefix: ! | This bot is still in it's early phases", bot.user.avatarURL)
       .setTimestamp();
+    message.reply("Check your DMs.")
     message.author.send(cmdsEmbed)
   } else
   if (cmd === `addrole`) {
